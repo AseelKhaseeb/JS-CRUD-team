af59821d70a118467a907c7947e715f72ec1175b
Raghad-sharaf <raghad.s.sharaf@gmail.com>
2022-07-07 00:03:18 +0300

    smooth scroll effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,40 @@
 //let backgroundImage = document.getElementById ('backgroundImage');
 //console.log(backgroundImage);
 
-let categoriesSection= $("#categories").offset().top;
-let productsSection= $("#products").offset().top;
-let discountSection= $("#discount").offset().top;
+//navbar change color start
+let categoriesOffset= $("#categories").offset().top;
+let productsOffset= $("#products").offset().top;
+let discountOffset= $("#discount").offset().top;
 $(window).scroll(function(){
     let tScroll = $(window).scrollTop();
 
-    if ((tScroll >=categoriesSection -50 ) && (tScroll<productsSection)){
+    if ((tScroll >=categoriesOffset -50 ) && (tScroll<productsOffset)){
         $(".navbar").css({"backgroundColor":"rgba(17, 17, 17,.7)","transition":"all .8s"});
+        $("#btnup").fadeIn(1000);
     }
-    else if((tScroll >=productsSection) && (tScroll<discountSection)){
+    else if((tScroll >=productsOffset) && (tScroll<discountOffset)){
         $(".navbar").css({"backgroundColor":"rgba(240, 128, 128,.8)","transition":"all .8s"});
     }
-    else if(tScroll >=discountSection){
+    else if(tScroll >=discountOffset){
         $(".navbar").css({"backgroundColor":"rgba(17, 17, 17,.7)","transition":"all .8s"});
     }
     else{
         $(".navbar").css("backgroundColor","#111111");
+        $("#btnup").fadeOut(1000);
     }
 });
+//navbar change color start
+
+//smooth scroll start
+$("#btnup").click(function(){
+    $(window).scrollTop(0);
+})
+//smooth scroll end
+
+//scroll behavior start
+$("a").click(function(e){
+    let attr=$(e.target).attr('href');
+    let sectionOffset= $(attr).offset().top;
+    $("html,body").animate({scrollTop:sectionOffset},2000);
+})
+//scroll behavior end
